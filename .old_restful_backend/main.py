a7876f4399baa7e9bea939d305f988e54cdaabff
Lars Dehlwes <ldehlwes@ethz.ch>
2023-08-21 17:47:21 -0300

    Migrated from flask RESTful to flask RESTx with autogenerated OpenAPI documentation

--- .old_restful_backend/main.py
+++ .old_restful_backend/main.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify, request, abort, Response
-from flask_restx import Api, Resource, reqparse
+import unidecode
+
+from flask import Flask, jsonify, request, abort
+from flask_restful import Resource, Api
 from flask_caching import Cache
+
+from marshmallow import Schema, fields, validate
 
 import json
 
@@ -17,19 +21,22 @@ config = {
     "CACHE_DEFAULT_TIMEOUT": 900
 }
 app = Flask(__name__)
-app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
-api = Api(app, version="0.1", title="Climatempo Talent REST API", description="Essa API fornece endpoints para acessar a autocompletação de nomes de cidades e a previsão do tempo.")
+api = Api(app)
 cache = Cache(app, config=config)
 
 # Argument parsing
 # The autocomplete_city endpoint allows for and requires only one argument: The user input string
-autocomplete_city_parser = reqparse.RequestParser()
-autocomplete_city_parser.add_argument('user_input', required=True, type=str, help='Nome incompleto da cidade digitado pelo usuário.')
-# The weatherforecast endpoint requires as input the city_id (obtained through the autocomplete_city endpoint) and the desired units for temperature and precipitation.
-weatherforecast_parser = reqparse.RequestParser()
-weatherforecast_parser.add_argument('city_id', required=True, type=int, help='Número identificador do local')
-weatherforecast_parser.add_argument('unit_temperature', required=True, type=str, help='Unidade de temperatura: "celsius" ou "fahrenheit".')
-weatherforecast_parser.add_argument('unit_precipitation', required=True, type=str, help='Unidade de precipitação: "mm" ou "inch".')
+class AutocompleteCityQuerySchema(Schema):
+    user_input = fields.String(required=True)
+
+# The weatherforecast endpoint requires as input the city_id (obtained through the autocomplete_city endpoint), the desired unit for .
+class WeatherForecastQuerySchema(Schema):
+    city_id = fields.Int(required=True)
+    unit_temperature = fields.String(required=True, validate=validate.OneOf(["celsius", "fahrenheit"]))
+    unit_precipitation = fields.String(required=True, validate=validate.OneOf(["mm", "inch"]))
+
+autocompletecity_schema = AutocompleteCityQuerySchema()
+weatherforecast_schema = WeatherForecastQuerySchema()
 
 def FahrenheitFromCelsius(temperature_celsius):
     if isinstance(temperature_celsius, str):
@@ -54,16 +61,15 @@ def nested_replace(structure, func, target_set_parent_key: set, target_set_key: 
     else:
         return structure
 
-    
-@api.route('/autocomplete_city')
 class AutocompleteCity(Resource):
     # Enable cache with 30 minutes timeout
-    @api.expect(autocomplete_city_parser)
     @cache.cached(timeout=1800, query_string=True)
     def get(self):
         # Validate parameters
-        args = autocomplete_city_parser.parse_args()
-        user_input_incomplete_city_name = args['user_input']
+        errors = autocompletecity_schema.validate(request.args)
+        if errors:
+            abort(400, str(errors))
+        user_input_incomplete_city_name = request.args.get('user_input')
         user_input_incomplete_city_name = unidecode(user_input_incomplete_city_name).lower()
         output = {}
         with open('base/locales.json', 'r') as city_options_file:
@@ -72,38 +78,35 @@ class AutocompleteCity(Resource):
         output['results'] = filtered
         return jsonify(output)
 
-@api.route('/weatherforecast')
 class WeatherForecast(Resource):
     # Enable cache with 5 minutes timeout
-    @api.expect(weatherforecast_parser)
     @cache.cached(timeout=300, query_string=True)
     def get(self):
         # Validate parameters
-        args = weatherforecast_parser.parse_args()
-        city_id = int(args['city_id'])
-        unit_temperature = args['unit_temperature']
-        unit_precipitation = args['unit_precipitation']
+        errors = weatherforecast_schema.validate(request.args)
+        if errors:
+            abort(400, str(errors))
+        city_id = int(request.args.get('city_id'))
+        unit_temperature = request.args.get('unit_temperature')
+        unit_precipitation = request.args.get('unit_precipitation')
         filtered = None
+        output = {}
         with open('base/weather.json', 'r') as forecast_file:
             forecast_json = json.load(forecast_file)
-            # If unit Fahrenheit is requested, substitute all temperature values according to the conversion formula.
+            # If unit Fahrenheit is requested, substitute all temperature values according to the conversion formula
             if unit_temperature == "fahrenheit":
                 forecast_json = nested_replace(forecast_json, FahrenheitFromCelsius, {"temperature"}, {"min", "max"})
-            elif unit_temperature == "celsius":
-                pass
-            else:
-                return Response(f'Wrong unit for temperature: {unit_temperature}. Must be one of "celsius" and "fahrenheit".', status=400)
-            # If unit Inch is requested, substitute all precipitation values according to the conversion formula.
             if unit_precipitation == "inch":
                 forecast_json = nested_replace(forecast_json, InchFromMillimeters, {"rain"}, {"precipitation"})
-            elif unit_precipitation == "mm":
-                pass
-            else:
-                return Response(f'Wrong unit for precipitation: {unit_precipitation}. Must be one of "mm" and "inch".', status=400)
             filtered = [forecast for forecast in forecast_json if city_id == forecast['locale']['id']]
         if filtered:
             return jsonify(filtered)
-        return Response(f'No weather forecast was found for your city. Please contact our support team and inform the requested city_id: {city_id}.', status=400)
+        output['error'] = f'No weather forecast was found for your city. Please contact our support team and inform the requested city_id: {city_id}.'
+        output['city_id'] = city_id
+        return jsonify(output)
+    
+api.add_resource(AutocompleteCity, '/autocomplete_city')
+api.add_resource(WeatherForecast, '/weatherforecast')
 
 if __name__ == '__main__':
     app.run(debug=True)
